3f2ab1f4572f60b4d7815f1c52c909cb68448381
stew3254 <rtstewy@gmail.com>
2018-09-25 15:08:43 -0400

    Fixed spaces and symbol checking

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 using std::cin;
 using std::cout;
 using std::endl;
+using std::getline;
 using std::isprint;
 using std::string;
 
@@ -33,28 +34,15 @@ public:
     string startingBoard = " . | . | . \n---|---|---\n . | . | . \n---|---|---\n . | . | . ";
 
     //Used to replace the values on the board
-    string update(const bool INITIAL=false)
+    string update()
     {
         short pos = 0;
-        string finalBoard;
-
-        //Check to initialize the values of positions correctly
-        if (INITIAL)
-        {
-            for (char c : positions)
-            {
-                c = ' ';
-            }
-        }
+        string finalBoard = startingBoard;
 
         //Used to place all of the symbols or spaces onto the board
-        for (char c : startingBoard)
+        while (finalBoard.find('.') >= 0 && finalBoard.find('.') <= finalBoard.length())
         {
-            if (c == '.')
-            {
-                c = positions[pos];
-            }
-            finalBoard += c;
+            finalBoard[finalBoard.find('.')] = positions[pos];
         }
         return finalBoard;
     }
@@ -93,7 +81,7 @@ public:
         cout << frame << endl;
         cout << "| " + TITLE + " |" << endl;
         cout << frame << endl;
-        cout << GameBoard.update(true) << endl;
+        cout << GameBoard.update() << endl;
         cout << frame << endl;
         cout << "  " << Player1.name << ", " << Player2.name << endl;
         cout << frame << endl;
@@ -104,16 +92,16 @@ public:
 
         //Get Player Names and Symbols
         cout << "Enter Player 1's Name: ";
-        cin >> Player1.name;
+        getline(cin, Player1.name);
 
         //Make sure the name isn't too long
         while (Player1.name.length() >= 20)
         {
             cout << "Please enter a name that's 20 characters or less: ";
-            cin >> Player1.name;
+            getline(cin, Player1.name);
         }
         cout << "Choose your symbol " + Player1.name + ": ";
-        cin >> Player1.symbol;
+        getline(cin, Player1.symbol);
 
         //Check to make sure that the symbol is printable
         bool symbolOkay = false;
@@ -122,17 +110,17 @@ public:
             if (Player1.symbol.length() != 1)
             {
                 cout << "Symbol must be only one character. Enter a new symbol: ";
-                cin >> Player1.symbol;
+                getline(cin, Player1.symbol);
             }
             else if (!isprint(Player1.symbol[0]))
             {
                 cout << "Symbol must be a printable character. Enter a new symbol: ";
-                cin >> Player1.symbol;
+                getline(cin, Player1.symbol);
             }
             else if (Player1.symbol == " ")
             {
                 cout << "Symbol cannot be a space. Enter a new symbol: ";
-                cin >> Player1.symbol;
+                getline(cin, Player1.symbol);
             }
             else
             {
@@ -141,7 +129,7 @@ public:
         }
 
         cout << "Enter Player 2's Name: ";
-        cin >> Player2.name;
+        getline(cin, Player2.name);
 
         //Again check length and then make sure they don't have the same name as Player1
         bool nameOkay = false;
@@ -150,12 +138,12 @@ public:
             if (Player2.name.length() >= 20)
             {
                 cout << "Please enter a name that's 20 characters or less. Enter a new name: ";
-                cin >> Player2.name;
+                getline(cin, Player2.name);
             }
             else if (Player2.name == Player1.name)
             {
                 cout << "Please don't choose the same name as Player1. Enter a new name: ";
-                cin >> Player2.name;
+                getline(cin, Player2.name);
             }
             else
             {
@@ -163,7 +151,7 @@ public:
             }
         }
         cout << "Choose your symbol " + Player2.name + ": ";
-        cin >> Player2.symbol;
+        getline(cin, Player2.symbol);
 
         //Check to make sure that the symbol is printable and not the same as the other player's
         symbolOkay = false;
@@ -172,21 +160,22 @@ public:
             if (Player2.symbol.length() != 1)
             {
                 cout << "Symbol must be only one character. Enter a new symbol: ";
-                cin >> Player2.symbol;
+                getline(cin, Player2.symbol);
             }
             else if (!isprint(Player2.symbol[0]))
             {
                 cout << "Symbol must be a printable character. Enter a new symbol: ";
-                cin >> Player2.symbol;
+                getline(cin, Player2.symbol);
             }
             else if (Player2.symbol == " ")
             {
                 cout << "Symbol cannot be a space. Enter a new symbol: ";
-                cin >> Player2.symbol;
+                getline(cin, Player2.symbol);
             }
             else if (Player2.symbol == Player1.symbol)
             {
                 cout << "Symbol cannot be the same as the other player. Enter a new symbol: ";
+                getline(cin, Player2.symbol);
             }
             else
             {
